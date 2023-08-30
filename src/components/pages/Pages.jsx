9477cb1fc50bd41cf632d2ddc977ec30b794f592
Oluwatobi <tobigtopsun@gmail.com>
2023-08-30 11:59:47 +0100

    first commit balconiesReactJS

---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../common/header/Header'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from '../home/Home';
+import Footer from '../common/footer/Footer';
+import About from '../about/About';
+
+const Pages = () => {
+  return (
+    <>
+    
+    <Router>
+        < Header />
+        <Switch>
+            <Route exact path = '/' component = {Home}/>
+            <Route exact path = '/about' component = {About}/>
+        </Switch>
+        < Footer />
+    </Router>
+    </>
+  )
+}
+
+export default Pages
